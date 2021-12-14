d801b029d4863a3a12e9b3145e081977010c3b92
Christian Vincent <christianjvincent318@gmail.com>
2021-12-13 19:22:31 -0500

    Transforming DOM HW

--- exercises/12-targeting-dom/targeting-dom.js
+++ exercises/12-targeting-dom/targeting-dom.js
@@ -11,6 +11,13 @@
  * Do not change the HTML unless you are instructed to do so.
  */
 (function () {
-  document.querySelector("<li>");
-  document.querySelectorAll("container mt-4 mb-4").forEach("btn btn-secondary");
+  document.querySelector("li");
+  document.querySelector("#myId");
+  document.querySelectorAll(".bg-warning");
+  document.querySelector(".btn-primary");
+  document.querySelectorAll(".link");
+  const cells = document.querySelectorAll("#myRow");
+  cells.forEach((cell) => {
+    console.log(cell);
+  });
 })();
--- exercises/13-transforming-dom/transforming-dom.js
+++ exercises/13-transforming-dom/transforming-dom.js
@@ -4,5 +4,22 @@
  * You may modify the HTML to add ids, classes, data attributes, etc.
  */
 (function () {
-  // Put your answers in here
+  const image = document.querySelector("img");
+  image.src = "https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif";
+  const newLink = document.querySelector(".myLink");
+  newLink.href = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+  const text = document.querySelector(".alert-success");
+  text.textContent = "I am victorious!";
+  const newBackgroundColor = document.querySelector(".alert-info");
+  newBackgroundColor.style.backgroundColor = "black";
+  const newTextColor = document.querySelector(".alert-danger");
+  newTextColor.style.color = "red";
+  const newHide = document.querySelector(".alert-warning");
+  newHide.style.display = "none";
+  const newDisplay = document.querySelector(".show");
+  newDisplay.style.display = "inline";
+  const checkText = document.querySelector(".text-dark");
+  if (checkText.classList.contains("btn-primary")) {
+    console.log("check blue");
+  }
 })();

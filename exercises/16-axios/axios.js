46a9389da2ea0419bd1dfd641f2eb699bf52bc48
Christian Vincent <christianjvincent318@gmail.com>
2021-12-27 18:46:57 -0500

    HW Axios

--- exercises/16-axios/axios.js
+++ exercises/16-axios/axios.js
@@ -12,4 +12,22 @@
    *    "message": "https://images.dog.ceo/breeds/poodle-toy/n02113624_9550.jpg"
    * }
    */
+   axios({
+    method: 'GET',
+    url: "https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css",
+  })
+  .then(response => {
+    // e.g. { data: data: { [{ first_name: ... },{ first_name: ... }] } }
+    response.data.data.forEach((user) => {
+      const { first_name, last_name, avatar } = user;
+      const htmlStr = `<img src="${avatar}"><p>${first_name} ${last_name}</p>`;
+      document.querySelector('.container')
+        .insertAdjacentHTML('beforeend', htmlStr); // Use a library to prevent XSS
+    });
+  })
+  .catch(err => {
+    console.error(err);
+    const htmlStr = 'no display';
+    document.querySelector(".container").innerHTML = htmlStr; // Use a library to prevent XSS
+  });
 })();
